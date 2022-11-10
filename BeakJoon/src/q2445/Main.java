c5993333978435622ceda9a3944f3e648f61233f
Administrator <Administrator@User-2022RPLKEB>
2022-11-10 18:05:46 +0900

    d

---
+++ BeakJoon/src/q2445/Main.java
@@ -0,0 +1,38 @@
+package q2445;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 1; j <= N * 2 - i * 2; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = 1; i <= N - 1; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 1; j <= i * 2; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
